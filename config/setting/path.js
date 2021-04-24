51c2c842c9c3545a079a461e2301fc869d716682
Kim Jae Hoon <hoon.development@gmail.com>
2021-04-24 20:31:14 +0900

    VIEWS_PATH to absolute path

--- config/setting/path.js
+++ config/setting/path.js
@@ -6,6 +6,9 @@
     desc: 주요 경로 설정파일
 */
 
+/* Node.js 모듈 */
+const path = require("path");
+
 module.exports = {
-    VIEWS_PATH: "../../src/views"
+    VIEWS_PATH: path.resolve(__dirname, "../../src/views")
 }
