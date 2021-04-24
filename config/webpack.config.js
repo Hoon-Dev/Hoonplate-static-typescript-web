ea9d0946158f47ade7c90e9d155a0bdadbf96dc6
Kim Jae Hoon <hoon.development@gmail.com>
2021-04-25 00:39:30 +0900

    install babel and typescript

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -33,6 +33,16 @@ module.exports = {
     },
     module:{
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
         ]
     },
     plugins:[
